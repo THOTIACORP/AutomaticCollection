7562e77871b7eaee479ab73315ca1188079d47e4
THOT I.A. CORP <90805434+THOTIACORP@users.noreply.github.com>
2025-12-05 21:08:08 -0400

    📚 docs: Readme.md

--- src/frontend/next-app/pages/index.jsx
+++
@@ -1,7 +0,0 @@
-import Link from 'next/link';
-export default function Home(){
-  return (<div style={{fontFamily:'system-ui',padding:20}}>
-    <h1>Escudo Orofacial - Frontend</h1>
-    <p><Link href='/captura'>Ir para captura</Link></p>
-  </div>)
-}
